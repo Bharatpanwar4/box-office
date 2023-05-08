3d2193b3b201312d42dfc95be410afbaa26d0012
Bharatpanwar4 <111150558+Bharatpanwar4@users.noreply.github.com>
2023-05-09 02:48:30 +0530

    added file

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home/>}/>
-      <Route path="/con" element={<Contact/>}/>
+      
 
     </Routes>
   </BrowserRouter>

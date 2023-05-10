42a857b0a945a717f6f2fd431dab3b4a52bf3470
Bharatpanwar4 <111150558+Bharatpanwar4@users.noreply.github.com>
2023-05-11 01:28:46 +0530

    fetch data

---
+++ src/api/tvmaze.js
@@ -0,0 +1,15 @@
+
+
+
+const BASE_URL=`https://api.tvmaze.com`;
+const apiGet = async(queryStr)=>
+{
+const response= await fetch(`${BASE_URL}${queryStr}`);
+const body = await response.json();
+
+return body;
+
+
+}
+
+export const searchForShows=(query)=>apiGet(`/search/shows?q=${query}`)

b6faf58b584749fb90b9eca3832da2cbd891a2f9
Bharatpanwar4 <111150558+Bharatpanwar4@users.noreply.github.com>
2023-05-11 19:45:12 +0530

    used query on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,27 +3,22 @@ import { searchForShows, searchForPeople } from '../api/tvmaze';
 import SearchForm from '../components/SearchForm';
 import ShowGrid from '../components/shows/ShowGrid';
 import ActorsGrid from '../components/actors/ActorsGrid';
+import { useQuery } from '@tanstack/react-query';
 
 const Home = () => {
-  const [apiData, setApiData] = useState(null);
-  const [apiDataError, setApiDataError] = useState(null);
 
-  //   https://api.tvmaze.com/search/shows?q=girls
+const [filter,setFilter]= useState(null)
+const {data:apiData,error:apiDataError}=useQuery({
+  queryKey:[`search`,filter],
+  queryFn:()=>filter.searchOption === `shows`?searchForShows(filter.q):searchForPeople(filter.q)
+  ,
+  enabled:!!filter,
+  refetchOnWindowFocus:false,
+})
+
   const onSearch = async ({ q, searchOption }) => {
-    try {
-      setApiDataError(null);
+    setFilter({q,searchOption})
 
-      let result;
-
-      if (searchOption === 'shows') {
-        result = await searchForShows(q);
-      } else {
-        result = await searchForPeople(q);
-      }
-      setApiData(result);
-    } catch (error) {
-      setApiDataError(error);
-    }
   };
 
   const renderApiData = () => {

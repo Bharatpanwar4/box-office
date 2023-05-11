32400ec333b24e2a5345c3a9a743c2d2273cb1fb
Bharatpanwar4 <111150558+Bharatpanwar4@users.noreply.github.com>
2023-05-11 19:29:41 +0530

    used fetch pakage dependencies @tansrack

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {QueryClient,QueryClientProvider} from '@tanstack/react-query'
 import Home from './pages/Home';
 import Starred from './pages/Starred';
 import PageNotFound from './pages/PageNotFound';
 import MainLayout from './components/MainLayout';
 import Show from './pages/Show';
+const queryClient = new QueryClient();
 function App() {
+
   return (
-    <BrowserRouter>
+   
+    <QueryClientProvider client={queryClient}>
+
+<BrowserRouter>
       <Routes>
         <Route element={<MainLayout />}>
           <Route path="/" element={<Home />} />
@@ -17,6 +23,8 @@ function App() {
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
+
+    </QueryClientProvider>
   );
 }
 
--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,24 +1,18 @@
-import { useEffect, useState } from 'react';
+
 import { useParams } from 'react-router-dom';
 import { getShowById } from '../api/tvmaze';
+import {  useQuery } from '@tanstack/react-query';
+
+
+
+
 
 const Show = () => {
-  const { showId } = useParams();
-  const [showData, setShowData] = useState(null);
-  const [showError, setShowError] = useState(null);
+    const { showId } = useParams();
 
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const data = await getShowById(showId);
-        setShowData(data);
-      } catch (err) {
-        setShowError(err);
-      }
-    }
+const {data:showData,error:showError} =useQuery({queryKey:[`show`,showId],
+queryFn:()=>getShowById(showId)});
 
-    fetchData();
-  }, [showId]);
 
   if (showError) {
     return <div>We have an error: {showError.message}</div>;

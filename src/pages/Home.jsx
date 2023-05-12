09906fc17b626c32a9a9acd9c1dc0fa6513d72c7
Bharatpanwar4 <111150558+Bharatpanwar4@users.noreply.github.com>
2023-05-12 13:31:35 +0530

    use useReducer and custum hook

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,33 +6,37 @@ import ActorsGrid from '../components/actors/ActorsGrid';
 import { useQuery } from '@tanstack/react-query';
 
 const Home = () => {
+  const [filter, setFilter] = useState(null);
 
-const [filter,setFilter]= useState(null)
-const {data:apiData,error:apiDataError}=useQuery({
-  queryKey:[`search`,filter],
-  queryFn:()=>filter.searchOption === `shows`?searchForShows(filter.q):searchForPeople(filter.q)
-  ,
-  enabled:!!filter,
-  refetchOnWindowFocus:false,
-})
+  const { data: apiData, error: apiDataError } = useQuery({
+    queryKey: [`search`, filter],
+    queryFn: () =>
+      filter.searchOption === `shows`
+        ? searchForShows(filter.q)
+        : searchForPeople(filter.q),
+    enabled: !!filter,
+    refetchOnWindowFocus: false,
+  });
 
   const onSearch = async ({ q, searchOption }) => {
-    setFilter({q,searchOption})
-
+    setFilter({ q, searchOption });
   };
 
   const renderApiData = () => {
     if (apiDataError) {
       return <div>Error occured: {apiDataError.message}</div>;
     }
-    if(apiData?.length===0){
-        return  <div>nors</div>
+    if (apiData?.length === 0) {
+      return <div>nors</div>;
     }
 
     if (apiData) {
-      return apiData[0].show
-        ? <ShowGrid shows={apiData}/>
-        : <ActorsGrid actors={apiData}/>;}
+      return apiData[0].show ? (
+        <ShowGrid shows={apiData} />
+      ) : (
+        <ActorsGrid actors={apiData} />
+      );
+    }
     return null;
   };
 
@@ -46,4 +50,4 @@ const {data:apiData,error:apiDataError}=useQuery({
 };
 
 export default Home;
-``
+``;
